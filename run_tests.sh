400bf7f87e26976ed77053f87001e4d6a700513c
rjun <rjun0729@gmail.com>
2023-12-25 13:53:04 +0800

    chore: update run_test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-TEST_DIR="./tests/public_test_data"
-ANS_FILE="./tests/public_test_data_ans.txt" # Replace with the actual path
-INTERPRETER="./mini-lisp"
+TEST_DIR="./tests/public_test_data" # TODO: 請修改成你的測試資料夾路徑
+ANS_FILE="./tests/public_test_data_ans.txt" # TODO: 請修改成你的測試答案檔案路徑
+PROGRAM_NAME="all_passed_Xyphuz"    # TODO: 請修改成你的執行檔名稱
+INTERPRETER="./bin/$PROGRAM_NAME" # Interpreter 執行檔路徑
+SRC="./src"
+BIN="./bin"
 PASSED=0
 FAILED=0
+
+# TODO: 可配置的編譯器和選項
+CC="gcc" # 可以更改為 g++ 或其他編譯器
+CFLAGS="-c -g -I."
+LFLAGS="-ll" # link with flex library
 
 # 顏色定義
 COLOR_BRIGHT_BLUE="\033[36m"
@@ -13,32 +21,76 @@ COLOR_RESET="\033[0m"
 COLOR_GREEN="\033[32m"
 COLOR_RED="\033[31m"
 COLOR_YELLOW="\033[33m"
-COLOR_RESET="\033[0m"
 
 # 清屏
 clear
 
 # 顯示基本訊息
 echo -e "${COLOR_BRIGHT_BLUE}+------------------------------------------------+${COLOR_RESET}"
+echo -e "${COLOR_BRIGHT_BLUE}|                                                |${COLOR_RESET}"
 echo -e "${COLOR_BRIGHT_BLUE}|     NCU Compilers Final Project, 2023 Fall     |${COLOR_RESET}"
 echo -e "${COLOR_BRIGHT_BLUE}|  Mini-LISP Interpreter crafted by xxrjun       |${COLOR_RESET}"
 echo -e "${COLOR_BRIGHT_BLUE}|                Running Tests                   |${COLOR_RESET}"
+echo -e "${COLOR_BRIGHT_BLUE}|                                                |${COLOR_RESET}"
+echo -e "${COLOR_BRIGHT_BLUE}|        Test Script created by @xxrjun          |${COLOR_RESET}"
+echo -e "${COLOR_BRIGHT_BLUE}|                                                |${COLOR_RESET}"
 echo -e "${COLOR_BRIGHT_BLUE}+------------------------------------------------+${COLOR_RESET}"
 
+# 檢查 src 和 bin 目錄是否存在，若不存在則創建
+mkdir -p $SRC
+mkdir -p $BIN
 
+# 選擇不同系統的指令
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux 系統的指令
+    echo "Running on Linux"
+
+    bison -d -o $SRC/${PROGRAM_NAME}.tab.c ${PROGRAM_NAME}.y
+    $CC $CFLAGS $SRC/${PROGRAM_NAME}.tab.c
+    lex -o $SRC/lex.yy.c ${PROGRAM_NAME}.l
+    $CC $CFLAGS $SRC/lex.yy.c
+    $CC -o $BIN/${PROGRAM_NAME} $SRC/${PROGRAM_NAME}.tab.o $SRC/lex.yy.o $LFLAGS
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS 系統的指令
+    echo "Running on macOS"
+    
+    bison -d -o $SRC/${PROGRAM_NAME}.tab.c ${PROGRAM_NAME}.y
+    $CC $CFLAGS $SRC/${PROGRAM_NAME}.tab.c
+    lex -o $SRC/lex.yy.c ${PROGRAM_NAME}.l
+    $CC $CFLAGS $SRC/lex.yy.c
+    $CC -o $BIN/${PROGRAM_NAME} $SRC/${PROGRAM_NAME}.tab.o $SRC/lex.yy.o $LFLAGS
+
+elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
+    # Windows 系統的指令 (使用 Cygwin 或 Git Bash)
+    echo "Running on Windows (Cygwin or Git Bash)"
+
+    bison -d -o $SRC/${PROGRAM_NAME}.tab.c ${PROGRAM_NAME}.y
+    $CC $CFLAGS $SRC/${PROGRAM_NAME}.tab.c
+    lex -o $SRC/lex.yy.c ${PROGRAM_NAME}.l
+    $CC $CFLAGS $SRC/lex.yy.c
+    $CC -o $BIN/${PROGRAM_NAME}.exe $SRC/${PROGRAM_NAME}.tab.o $SRC/lex.yy.o $LFLAGS
+
+else
+    echo "Unknown operating system"
+fi
+
+# 測試
 for test_file in $TEST_DIR/*.lsp; do
     test_name=$(basename $test_file)
 
     # Extract the expected output from the answer file
     expected_output=$(awk "/^$test_name$/{flag=1;next}/^[a-zA-Z0-9]+_[0-9]+\.lsp$/{flag=0}flag" "$ANS_FILE")
-    actual_output=$($INTERPRETER < $test_file)
-
-    # Handle multiple possible outputs
-    if [[ "$expected_output" =~ .*"syntax error".* ]]; then
-        if [[ "$actual_output" == "syntax error" ]] || [[ "$actual_output" == "Need 2 arguments, but got 0." ]]; then
-            actual_output="$expected_output"
-        fi
+    if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "darwin"* ]]; then
+        actual_output=$($INTERPRETER < $test_file)
+    elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
+        actual_output=$($INTERPRETER.exe < $test_file)
+    else
+        echo "Unsupported operating system to run tests: $OSTYPE"
+        exit 1
     fi
+
+    expected_output=$(echo "$expected_output" | tr -d '\r')
+    actual_output=$(echo "$actual_output" | tr -d '\r')
 
     echo -e "\033[36mTest: $test_name\033[0m"
 
@@ -58,3 +110,6 @@ for test_file in $TEST_DIR/*.lsp; do
 done
 
 echo -e "${COLOR_YELLOW}Tests completed.${COLOR_RESET} ${COLOR_GREEN}Passed: $PASSED${COLOR_RESET}, ${COLOR_RED}Failed: $FAILED${COLOR_RESET}"
+
+# 清除除了執行檔外的所有生成檔案
+rm -f $SRC/*.tab.c $SRC/*.tab.h $SRC/*.yy.c $SRC/*.output $SRC/*.o
